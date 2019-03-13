e819d2f33acfd6eed61c3b4879ed625c08fa1924
Dmitry Serediuk <k0sm0s@tut.by>
2019-03-13 18:25:42 +0300

    feat: all test is pass

--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
+/**
+ * Get count zero in the end of Number
+ * @param {*} number Nuber for cheking zero
+ * @param {*} simpleNumber Simple number of base
+ * @param {*} countSimple Count repeat simple number
+ */
+function sumRecBase(number, simpleNumber, countSimple){
+  var fullZero = 0;
+  while(number = Math.trunc(number/simpleNumber)){
+    fullZero += number;
+  }
+
+  return Math.trunc(fullZero/countSimple);
+}
+
+/**
+ * Check all simple numbers of base
+ * @param {integer} number Nuber for cheking zero
+ * @param {Array} arrSimple Array simple nubers by base
+ * @returns {Array} List of count zero;
+ */
+function checkArrayBaseSumZero(number,arrSimple){
+  var countZero = [];
+  for (var simpleNumber in arrSimple) {
+    countZero.push(sumRecBase(number, simpleNumber, arrSimple[simpleNumber]));
+  }
+
+  return countZero;
+}
+
+
+/**
+ * Form array whith simple numbers and count it
+ * @param {integer} number Number for find simple value
+ * @returns {Array} returnArrSimple Array whith simple numbers
+ */
+
+function getArrSimple(number){
+  var arrSimple = {};
+  var i = 2;
+  while (i <= number){
+    if (number % i == 0){
+      if (i in arrSimple){
+        arrSimple[i]++;
+      }else{
+        arrSimple[i] = 1;
+      }
+      number = number/i;
+    }else{
+        i++;
+    }
+  }
+  return arrSimple;
+}
+
+/**
+ * Main function
+ */
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+   let countZero = Math.min.apply(null, checkArrayBaseSumZero(number,getArrSimple(base)));
+  return countZero;
 }
